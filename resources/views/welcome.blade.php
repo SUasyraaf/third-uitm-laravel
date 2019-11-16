d8ba255cec424cddb50832e47d728c8e7c1d492f
SUasyraaf <SUasyraaf>
2019-11-17 06:07:46 +0800

    updated title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Acap Deployed!
                 </div>
 
                 <div class="links">
